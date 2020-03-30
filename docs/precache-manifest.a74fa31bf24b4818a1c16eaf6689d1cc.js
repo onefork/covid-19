b93c400203ea7df43f5c5de292e516dc94b59bc9
Bin <onefork@gmail.com>
2020-03-30 21:18:18 +0200

    GitHub page

---
+++ docs/precache-manifest.a74fa31bf24b4818a1c16eaf6689d1cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d840d2ce52cc5ff58785301f3df1b6bc",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "315abb8a9b8964db25e0",
+    "url": "/covid-19/static/css/main.c58aae92.chunk.css"
+  },
+  {
+    "revision": "b2438e3959007b6478b0",
+    "url": "/covid-19/static/js/2.28437aa3.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/covid-19/static/js/2.28437aa3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "315abb8a9b8964db25e0",
+    "url": "/covid-19/static/js/main.912c3216.chunk.js"
+  },
+  {
+    "revision": "30cb0f5c7b2e673f49f2",
+    "url": "/covid-19/static/js/runtime-main.77452d7e.js"
+  }
+]);
