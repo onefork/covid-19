7298046b6fcb1139ed0f1f3acc6db1b90b7e9f9b
Bin <onefork@gmail.com>
2020-04-06 12:47:37 +0200

    Version 1

---
+++ precache-manifest.3c5996ab57328b318360a1196c002e76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd1af01fcc035599ece1b0c2d5945dff",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "153ad1a6264c04644d5a",
+    "url": "/covid-19/static/css/main.c58aae92.chunk.css"
+  },
+  {
+    "revision": "7cd3af49dde6cac48c1c",
+    "url": "/covid-19/static/js/2.9ae19045.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/covid-19/static/js/2.9ae19045.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "153ad1a6264c04644d5a",
+    "url": "/covid-19/static/js/main.94c8222c.chunk.js"
+  },
+  {
+    "revision": "30cb0f5c7b2e673f49f2",
+    "url": "/covid-19/static/js/runtime-main.77452d7e.js"
+  }
+]);
