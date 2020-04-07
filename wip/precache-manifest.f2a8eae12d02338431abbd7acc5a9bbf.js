8c2a885b14c98f3d5e4a919b2508e8b66300cb33
Bin <onefork@gmail.com>
2020-04-07 22:04:54 +0200

    Test localhost as backend

---
+++ wip/precache-manifest.f2a8eae12d02338431abbd7acc5a9bbf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87658b6c32a627acb4b085f668b07812",
+    "url": "https://onefork.github.io/covid-19/wip/index.html"
+  },
+  {
+    "revision": "b338885c2d304632b979",
+    "url": "https://onefork.github.io/covid-19/wip/static/css/main.93ac47a6.chunk.css"
+  },
+  {
+    "revision": "f7ff0c0ba47a698b3dcd",
+    "url": "https://onefork.github.io/covid-19/wip/static/js/2.7647795b.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "https://onefork.github.io/covid-19/wip/static/js/2.7647795b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b338885c2d304632b979",
+    "url": "https://onefork.github.io/covid-19/wip/static/js/main.94b44319.chunk.js"
+  },
+  {
+    "revision": "1b667871c8f245f29497",
+    "url": "https://onefork.github.io/covid-19/wip/static/js/runtime-main.59a00c33.js"
+  },
+  {
+    "revision": "e372b9db1c823d6b3d0b8d3ff950c34f",
+    "url": "https://onefork.github.io/covid-19/wip/static/media/ColorWheel.e372b9db.png"
+  },
+  {
+    "revision": "19ebd9a2ba056f91ef72bda2af187ada",
+    "url": "https://onefork.github.io/covid-19/wip/static/media/curvedline.19ebd9a2.png"
+  }
+]);
